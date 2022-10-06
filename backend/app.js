de3ac63f25dfaceb704b984002677e0a412671ee
Sushanta Bhowmick <bhosushanta922@gmail.com>
2022-10-06 20:09:31 +0530

    initial changes

--- backend/app.js
+++ backend/app.js
@@ -19,10 +19,10 @@ app.use(cookieParser());
  app.use("/api/v1",post);
  app.use("/api/v1",user);
 
-//  app.use(express.static(path.join(__dirname, "../frontend/build")));
+ app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
  module.exports = app;
